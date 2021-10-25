206723f099b3aeb2ccb1d403ed2567d84472967e
jean_lc <jl_chartrand@hotmail.com>
2021-10-25 08:28:38 -0400

    type stability

--- src/Standard/main.jl
+++ src/Standard/main.jl
@@ -1,7 +1,7 @@
 abstract type AbstractStandard{T} end
 
-function Simplexe(A::Array{T, 2}, b::Array{S, 1}, c::Array{W, 1}; method::Type{AbstractStandard},
-        verbose::Bool = false) where {T, S, W}
+function Simplexe(A::Array{T, 2}, b::Array{S, 1}, c::Array{W, 1}; method::Type{METHODE},
+        verbose::Bool = false) where {METHODE <: AbstractStandard, T, S, W}
     nT = promote_type(T, S, W)
     return method(Array{nT, 2}(A), Array{nT, 1}(b), Array{nT, 1}(c), verbose = verbose)
 end
