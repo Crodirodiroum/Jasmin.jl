eb9e0a91eeeb5ee65c43e20ccaea299c46fa3902
jean_lc <jl_chartrand@hotmail.com>
2021-10-27 22:40:48 -0400

    Update main.jl

--- src/Standard/main.jl
+++ src/Standard/main.jl
@@ -3,7 +3,7 @@ abstract type AbstractStandard{T} end
 function Simplexe(A::Array{T, 2}, b::Array{S, 1}, c::Array{W, 1}; method::Type{METHODE} = StandardSimplexe,
         verbose::Bool = false) where {METHODE <: AbstractStandard, T, S, W}
     nT = promote_type(T, S, W)
-    supertype(nT) != Real && nT = Float64
+    !(supertype(nT) <: Real) && (nT = Float64)
     return method(Array{nT, 2}(A), Array{nT, 1}(b), Array{nT, 1}(c), verbose = verbose)
 end
     
